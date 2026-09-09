99612fdb1a6f338395cedc886f5798b005307cc4
tryit62 <pinard.johan@icloud.com>
2026-09-09 14:30:20 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,10 @@
-const CACHE='ordre-terminal-v181-cartographie-planisphere';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.webmanifest','./order-logo.png','./apple-touch-icon.png','./icon-192.png','./icon-512.png',
-  './division-1-oeil-fendu.png',
-  './division-2-flamme-inversee.png',
-  './division-3-main-cassee.png',
-  './division-4-spirale-os.png',
-  './division-5-sablier-noir.png'
+const CACHE='ordre-terminal-v190-ceremonie-integree';
+const ASSETS=[
+'./','./index.html','./style.css','./app.js','./manifest.webmanifest','./service-worker.js',
+'./order-logo.png','./apple-touch-icon.png','./icon-192.png','./icon-512.png',
+'./division-1-oeil-fendu.png','./division-2-flamme-inversee.png','./division-3-main-cassee.png',
+'./division-4-spirale-os.png','./division-5-sablier-noir.png',
+'./ceremony.html','./ceremonie.mp3','./oeil.png','./flamme.png','./main.png','./spirale.png','./sablier.png','./ordre.png'
 ];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
